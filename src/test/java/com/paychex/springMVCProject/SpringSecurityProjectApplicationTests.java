414b43c70859e8c6d6ee88963ee8486d62daccec
Ronil <ronilsahoo13@gmail.com>
2020-06-30 13:08:43 +0530

    first commit to implement spring security in spring boot project

---
+++ src/test/java/com/paychex/springMVCProject/SpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paychex.springMVCProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
