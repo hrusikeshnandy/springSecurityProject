2c17faa9ffa224ca609790e58845064d2e39828c
Ronil <ronilsahoo13@gmail.com>
2020-06-30 15:58:27 +0530

    implemented the MySQL connection to validate the user log in

--- src/main/java/com/paychex/springSecurity/AppSpringSecurityConfig.java
+++ src/main/java/com/paychex/springSecurity/AppSpringSecurityConfig.java
@@ -3,28 +3,47 @@ package com.paychex.springSecurity;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSpringSecurityConfig extends WebSecurityConfigurerAdapter{
-	
+	@Autowired
+	UserDetailsService userDetailService;
 	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		List<UserDetails> users = new ArrayList<>();
+	public AuthenticationProvider authProvider() {
 		
-		users.add(User.withDefaultPasswordEncoder().username("hrusi").password("1234").roles("USER").build());
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailService);
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
 		
-		return new InMemoryUserDetailsManager(users);
+		return provider;
 	}
+	
+	
+	
+	/*
+	 * @Bean
+	 * 
+	 * @Override protected UserDetailsService userDetailsService() {
+	 * List<UserDetails> users = new ArrayList<>();
+	 * 
+	 * users.add(User.withDefaultPasswordEncoder().username("hrusi").password("1234"
+	 * ).roles("USER").build());
+	 * 
+	 * return new InMemoryUserDetailsManager(users); }
+	 */
 	
 	
 }
---
+++ src/main/java/com/paychex/springSecurity/UserRepo.java
@@ -0,0 +1,7 @@
+package com.paychex.springSecurity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer>{
+	 User findByUsername(String username);
+}
